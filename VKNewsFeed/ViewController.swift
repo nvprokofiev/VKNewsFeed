16a8235dcd2ffa549d67370e9b354ce9154ef935
NikolaiProkofev <nprokofev@novoconn.com>
2019-07-08 16:38:06 -0400

    Initial Commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by Nikolai Prokofev on 2019-07-08.
+//  Copyright © 2019 Nikolai Prokofev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
